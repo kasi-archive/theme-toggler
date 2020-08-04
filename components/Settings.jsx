c6ecdfbe1e6ca7e13363c96073a0ad15e0e4e8d3
redstonekasi <redstonekasi@gmail.com>
2020-08-04 08:38:40 +0200

    add enable / disable all buttons

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -1,5 +1,5 @@
 const { React, i18n: { Messages } } = require('powercord/webpack');
-const { Divider } = require('powercord/components');
+const { Divider, Button } = require('powercord/components');
 
 const InstalledProduct = require('../../pc-moduleManager/components/parts/InstalledProduct');
 
@@ -13,6 +13,14 @@ module.exports = class ThemeTogglerSettings extends React.Component {
             <div className='powercord-entities-manage powercord-text'>
                 <div className='powercord-entities-manage-header'>
                     <span>{Messages[`POWERCORD_THEMES_INSTALLED`]}</span>
+                    <div className='buttons theme-toggler-buttons'>
+                        <Button onClick={() => this.enableAll()} color={Button.Colors.GREEN} look={Button.Looks.FILLED} size={Button.Sizes.SMALL}>
+                            {Messages.THEME_TOGGLER_ENABLE_ALL}
+                        </Button>
+                        <Button onClick={() => this.disableAll()} color={Button.Colors.RED} look={Button.Looks.FILLED} size={Button.Sizes.SMALL}>
+                            {Messages.THEME_TOGGLER_DISABLE_ALL}
+                        </Button>
+                    </div>
                 </div>
                 <Divider/>
                 {this.renderBody()}
@@ -42,5 +50,15 @@ module.exports = class ThemeTogglerSettings extends React.Component {
                 }}
             />
         );
+    }
+
+    enableAll() {
+        powercord.styleManager.themes.forEach(theme =>
+            powercord.styleManager.enable(theme.entityID));
+    }
+
+    disableAll() {
+        powercord.styleManager.themes.forEach(theme =>
+            powercord.styleManager.disable(theme.entityID));
     }
 }
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const { Plugin } = require('powercord/entities');
 
 const Settings = require('./components/Settings.jsx');
+const i18n = require('./i18n');
 
 module.exports = class ThemeToggler extends Plugin {
     startPlugin() {
+        this.loadStylesheet('style.scss');
+        powercord.api.i18n.loadAllStrings(i18n);
         powercord.api.settings.registerSettings('theme-toggler', {
             category: this.entityID,
             label: 'Theme Toggler',
