b8b75ed8f1c24a44a58bda324d799d74331b553b
A-Trash-Coder <gavynlamar@gmail.com>
2020-10-15 20:24:48 -0400

    Add theme enabler/disabler

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const { Plugin } = require('powercord/entities');
 
 const Settings = require('./components/Settings.jsx');
 const i18n = require('./i18n');
+const commands = require('./commands');
 
 module.exports = class ThemeToggler extends Plugin {
     startPlugin() {
+        Object.values(commands).forEach(cmd => powercord.api.commands.registerCommand(cmd));
         this.loadStylesheet('style.scss');
         powercord.api.i18n.loadAllStrings(i18n);
         powercord.api.settings.registerSettings('theme-toggler', {
@@ -16,5 +18,6 @@ module.exports = class ThemeToggler extends Plugin {
 
     pluginWillUnload() {
         powercord.api.settings.unregisterSettings('theme-toggler');
+        Object.values(commands).forEach(cmd => powercord.api.commands.unregisterCommand(cmd.command));
     }
 }
